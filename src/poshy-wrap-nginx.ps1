eeb69c1de1c3f862e789ad3f19b9fd6007323508
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-18 13:39:16 -0500

    🏗️ Splits PSM1, Removes `#Requires` from PSM1
    Tests were failing because the module was not importable. The `#Requires` syntax being used in the root PSM1 prevented dependencies from being fully resolved before the module loading could complete, making the module unusable.

---
+++ src/poshy-wrap-nginx.ps1
@@ -0,0 +1,81 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+#Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
+
+
+<#
+.SYNOPSIS
+    Reload your nginx config.
+.COMPONENT
+    nginx
+#>
+function nginx_reload {
+    if (-not $Env:NGINX_PATH) {
+        throw "`$Env:NGINX_PATH not set"
+    }
+    $FILE="${Env:NGINX_PATH}/logs/nginx.pid"
+    if (Test-Path $FILE -ErrorAction SilentlyContinue) {
+        Write-Output "Reloading NGINX…"
+        $nginx_pid = (Get-Content $FILE).Trim()
+        sudo kill -HUP $nginx_pid
+    } else {
+        Write-Error "Nginx pid file not found"
+    }
+}
+
+<#
+.SYNOPSIS
+    Stop nginx.
+.COMPONENT
+    nginx
+#>
+function nginx_stop {
+    if (-not $Env:NGINX_PATH) {
+        throw "`$Env:NGINX_PATH not set"
+    }
+    $FILE="${Env:NGINX_PATH}/logs/nginx.pid"
+    if (Test-Path $FILE -ErrorAction SilentlyContinue) {
+        Write-Output "Stopping NGINX…"
+        $nginx_pid = (Get-Content $FILE).Trim()
+        sudo kill -INT $nginx_pid
+    } else {
+        Write-Error "Nginx pid file not found"
+    }
+}
+
+<#
+.SYNOPSIS
+    Start nginx.
+.COMPONENT
+    nginx
+#>
+function nginx_start {
+    if (-not $Env:NGINX_PATH) {
+        throw "`$Env:NGINX_PATH not set"
+    }
+    $FILE="${Env:NGINX_PATH}/sbin/nginx"
+    if (Test-x $FILE) {
+        Write-Output "Starting NGINX…"
+        sudo "${FILE}"
+    } else {
+        Write-Error "Couldn't start nginx"
+    }
+}
+
+<#
+.SYNOPSIS
+    Restart nginx.
+.COMPONENT
+    nginx
+#>
+function nginx_restart() {
+    nginx_stop
+    if ($LASTEXITCODE -ne 0) {
+        return
+    }
+    nginx_start
+}
+
+
+Export-ModuleMember -Function * -Alias *
--- src/poshy-wrap-nginx.psm1
+++ src/poshy-wrap-nginx.psm1
@@ -1,85 +1,10 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
-#Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
-if (-not (Test-Command nginx) -and (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue))) {
+if (-not (Get-Command nginx -ErrorAction SilentlyContinue) -and (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue))) {
     return
+} else {
+    . "$PSScriptRoot/poshy-wrap-nginx.ps1"
 }
-
-<#
-.SYNOPSIS
-    Reload your nginx config.
-.COMPONENT
-    nginx
-#>
-function nginx_reload {
-    if (-not $Env:NGINX_PATH) {
-        throw "`$Env:NGINX_PATH not set"
-    }
-    $FILE="${Env:NGINX_PATH}/logs/nginx.pid"
-    if (Test-Path $FILE -ErrorAction SilentlyContinue) {
-        Write-Output "Reloading NGINX…"
-        $nginx_pid = (Get-Content $FILE).Trim()
-        sudo kill -HUP $nginx_pid
-    } else {
-        Write-Error "Nginx pid file not found"
-    }
-}
-
-<#
-.SYNOPSIS
-    Stop nginx.
-.COMPONENT
-    nginx
-#>
-function nginx_stop {
-    if (-not $Env:NGINX_PATH) {
-        throw "`$Env:NGINX_PATH not set"
-    }
-    $FILE="${Env:NGINX_PATH}/logs/nginx.pid"
-    if (Test-Path $FILE -ErrorAction SilentlyContinue) {
-        Write-Output "Stopping NGINX…"
-        $nginx_pid = (Get-Content $FILE).Trim()
-        sudo kill -INT $nginx_pid
-    } else {
-        Write-Error "Nginx pid file not found"
-    }
-}
-
-<#
-.SYNOPSIS
-    Start nginx.
-.COMPONENT
-    nginx
-#>
-function nginx_start {
-    if (-not $Env:NGINX_PATH) {
-        throw "`$Env:NGINX_PATH not set"
-    }
-    $FILE="${Env:NGINX_PATH}/sbin/nginx"
-    if (Test-x $FILE) {
-        Write-Output "Starting NGINX…"
-        sudo "${FILE}"
-    } else {
-        Write-Error "Couldn't start nginx"
-    }
-}
-
-<#
-.SYNOPSIS
-    Restart nginx.
-.COMPONENT
-    nginx
-#>
-function nginx_restart() {
-    nginx_stop
-    if ($LASTEXITCODE -ne 0) {
-        return
-    }
-    nginx_start
-}
-
-
-Export-ModuleMember -Function * -Alias *
